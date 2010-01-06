1b879ca981665ebf064808852f5d20a36176a5ac
stahnma <stahnma@websages.com>
2010-01-06 04:44:01 +0000

    Slightly more worthless ideas.

--- go.rb
+++ go.rb
@@ -12,10 +12,16 @@ require 'yaml'
 
 #rpm -q --qf '%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}|%{DSAHEADER:pgpsig}\n' -a
 
+PACKAGE_WATCH_DIR="incoming"
+END_PACKAGE_DIR="repo"
 GOOD_PUBLIC_KEYS_DIRECTORY='/etc/pki/rpm'
 OFFICIAL_KEY='mykey.gpg'
 
 def get_packages_available
+   Dir[PACKAGE_WATCH_DIR + "/*"].each do |packagefile|
+     puts packagefile
+     # check it ends in .deb or .rpm
+   end
 end
 
 def check_package
@@ -26,3 +32,5 @@ end
 
 def throw_away
 end
+
+get_packages_available
