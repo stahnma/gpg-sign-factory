2ff6442e3a4a474719dd7881f27213202b703b29
stahnma <stahnma@websages.com>
2010-01-07 05:00:36 +0000

    Test stuff

--- go.rb
+++ go.rb
@@ -17,20 +17,29 @@ END_PACKAGE_DIR="repo"
 GOOD_PUBLIC_KEYS_DIRECTORY='/etc/pki/rpm'
 OFFICIAL_KEY='mykey.gpg'
 
+# no package
+# packages not signed
+# package signed by wrong person
 def get_packages_available
    Dir[PACKAGE_WATCH_DIR + "/*"].each do |packagefile|
      puts packagefile
      # check it ends in .deb or .rpm
+     unless packagefile =~ /rpm$/
+        throw_away(packagefile)
+     else
+        check_package(packagefile)
+     end
    end
 end
 
-def check_package
+def check_package(pkg)
+   ` rpm -qp --qf '%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}|%{DSAHEADER:pgpsig}\n' #{pkg}`
 end
 
 def good_sign
 end
 
-def throw_away
+def throw_away(pkg)
 end
 
 get_packages_available
