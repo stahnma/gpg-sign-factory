008f4c4a409ee6a587a93922601214a0fb86cb9b
stahnma <stahnma@websages.com>
2010-01-07 21:18:48 -0600

    Starting work on keyring to figure out signings.

---
+++ keyring.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+
+# Load existing gpg keys from keyring
+# 
+class Keyring
+  def initialize
+    # Do a gpg --list-keys and parse?
+  end
+end
+
+
+class Keypair
+  def initialize
+    @email = "" 
+    @keyid = ""
+  end
+end
